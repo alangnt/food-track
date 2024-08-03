a03d6c24c8533bb0a82a08a4672d67e333fdb429
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-08-03 13:34:39 +0200

    gets analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Container } from "@mui/material";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}><Container sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>{children}</Container></body>
+      <Analytics />
     </html>
   );
 }
